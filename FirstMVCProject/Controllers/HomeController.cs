92537e3eec18c7bc6a8b0359b2d6b7cf5e948485
Kal Vassar <kvassar@hbs.net>
2022-10-13 12:53:00 -0500

    Started a new project were I am learning about MVC

---
+++ FirstMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("https://www.bang-olufsen.com/en/us");
+        }
+
+        public ActionResult FirstAction()
+        {
+            return View();
+        }
+        public ActionResult GoToProduct()
+        {
+            return RedirectToAction("AddProduct", "Product");
+
+        }
+        public ActionResult GoToUpdateProduct()
+        {
+            return RedirectToAction("updateProduct", "Product", new { ID = 20 });
+        }
+    }
+}
